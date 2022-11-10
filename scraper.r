b10ceccfe55b1088815a3596cb0ba39c957ef78b
JBergMSDS <112781351+JBergMSDS@users.noreply.github.com>
2022-11-10 10:13:20 -0800

    Update scraper.r

--- scraper.r
+++ scraper.r
@@ -40,7 +40,25 @@ con <- dbConnect(
 
 ##Infinite loop on 24 hr sleep cycle
 while(TRUE){
-  f.get_forex_rates()
+  url <- "https://exchange-rates-api.oanda.com/v1/rates/USD.json?api_key=lNqMjgn1qSAtIWmh72xQApCb&quote=CAD&quote=JPY&quote=AUD&quote=EUR&quote=GBP&fields=all&data_set=OANDA"
+  response <- read_html(url) %>% html_text() %>% fromJSON()
+  quote <- response[3]
+  quotes <- quote$quotes
+  AUD <- as.data.frame(quotes$AUD)
+  AUD %<>% mutate(date = as.Date(date))
+  return(AUD)
+  JPY <- as.data.frame(quotes$JPY)
+  JPY %<>% mutate(date = as.Date(date))
+  return(JPY)
+  CAD <- as.data.frame(quotes$CAD)
+  CAD %<>% mutate(date = as.Date(date))
+  return(CAD)
+  EUR <- as.data.frame(quotes$EUR)
+  EUR %<>% mutate(date = as.Date(date))
+  return(EUR)
+  GBP <- as.data.frame(quotes$GBP)
+  GBP %<>% mutate(date = as.Date(date))
+  return(GBP)
   ##Table creation and data entry
   dbExecute(con, "CREATE TABLE IF NOT EXISTS forex_rates (
             id SERIAL PRIMARY KEY,
